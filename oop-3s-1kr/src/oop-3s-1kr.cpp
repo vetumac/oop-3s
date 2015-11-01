2446a11e05bbb86f9520f975712a84c3d99fc297
Alexey Suslov <alexei.suslov@gmail.com>
2015-11-01 22:15:17 +0300

    init commit

---
+++ oop-3s-1kr/src/oop-3s-1kr.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : oop-3s-1kr.cpp
+// Author      : Alexey Suslov
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
