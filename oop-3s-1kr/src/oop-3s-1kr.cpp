9789c691f011b0aadb8c0741898dc87b1277ed19
Alexey Suslov <alexei.suslov@gmail.com>
2015-11-03 23:35:21 +0300

    b and c done

--- oop-3s-1kr/src/oop-3s-1kr.cpp
+++ oop-3s-1kr/src/oop-3s-1kr.cpp
@@ -11,27 +11,34 @@
 using namespace std;
 
 class Word {
-	char word[4];
+private:
+	char word[30];
 	int pages[10];
 	int pagesCount;
 public:
 	Word();
+	Word(char[30]);
 	int* getPages();
 	int getPagesCount();
 	char* getWord();
 	void out();
 	int compareTo(Word);
+	bool equalsWord(Word);
 };
 
 Word::Word() {
-	cout << "Input word: " << endl;
+	cout << "Input word: ";
 	cin >> word;
-	cout << "Input pages with word count <= 10: " << endl;
+	cout << "Input pages with word count <= 10: ";
 	cin >> pagesCount;
 	for (int i = 0; i < pagesCount; i++) {
-		cout << "Input pages with word count" << endl;
+		cout << "Input pages with word : ";
 		cin >> pages[i];
 	}
+}
+
+Word::Word(char anotherWord[30]) {
+	strcpy(word, anotherWord);
 }
 
 int* Word::getPages() {
@@ -48,27 +55,31 @@ char* Word::getWord() {
 
 void Word::out() {
 	cout << "Word: " << word << endl;
-	cout << "Pages with word count: " << pagesCount << endl;
+	cout << "Pages with word count : " << pagesCount << endl;
+	cout << "Pages with word : ";
 	for (int i = 0; i < pagesCount; i++) {
-		cout << "Pages with word count: " << pages[i] << " " << endl;
+		cout << pages[i] << " ";
 	}
-	cout<<endl<<endl;
+	cout << endl;
 }
 
 int Word::compareTo(Word thatWord) {
 	int thatWordLen = strlen(thatWord.word);
 	int thisWordLen = strlen(word);
 
-	if (thisWordLen < thatWordLen) return -1;
-	if (thisWordLen > thatWordLen) return 1;
-
 	int i = 0;
 	while (i < thisWordLen && i < thatWordLen) {
-		if (word[i] < thatWord.word[i]) return -1;
-		if (word[i] > thatWord.word[i]) return 1;
+		if (word[i] < thatWord.getWord()[i]) return -1;
+		if (word[i] > thatWord.getWord()[i]) return 1;
 		i++;
 	}
+	if (thisWordLen < thatWordLen) return -1;
+	if (thisWordLen > thatWordLen) return 1;
 	return  0;
+}
+
+bool Word::equalsWord(Word thatWord) {
+	return compareTo(thatWord) == 0 ? true : false;
 }
 
 void quickSort(Word a[], int first, int last)
@@ -92,27 +103,38 @@ void quickSort(Word a[], int first, int last)
 }
 
 int main() {
+	// create array of Words
 	int wordsCount;
-	cout << "Input word count: " << endl; cin>>wordsCount;
-
-	Word *words;
-	words = new Word[wordsCount];
-
+	cout << "Input word count: ";
+	cin >> wordsCount;
+	Word words[wordsCount];
 	int minPageCount;
-	cout << "Input minimum page with word count (N) <= 10: " << endl;
+	cout << "Input minimum page with word count (N) <= 10: ";
 	cin >> minPageCount;
 
-	cout << "Words in more then N pages:" << endl;
+	// a)
+	cout << "Words in more then N pages: ";
 	for (int i = 0; i < wordsCount; i++) {
 		if (words[i].getPagesCount() > minPageCount) {
 			words[i].out();
 		}
 	}
 
+	// b)
 	quickSort(words, 0, wordsCount - 1);
-
 	for (int i = 0; i < wordsCount; i++) {
 		words[i].out();
+	}
+
+	// c)
+	char word[30];
+	cout << "Input word to compare: ";
+	cin >> word;
+	Word* wordNew = new Word(word);
+	for (int i = 0; i < wordsCount; i++) {
+		if (words[i].equalsWord(*wordNew)) {
+			words[i].out();
+		}
 	}
 
 	return 0;
